c00f270eba2bef7c05787044af4049709c8af3e3
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-06-04 14:51:43 -0400

    Add example gene set libraries

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' ENCODE gene set library
+#'
+#' May be used for genesetr examples.
+#'
+#' A list of gene sets from transcription factor
+#' ChIP-seq studies conducted by the ENCODE consortium.
+#' Downloaded from the web tool Enrichr on Aug. 23, 2017.
+#' ENCODE_TF_ChIP-seq_2015.txt
+#'
+#' @format A list of 816 character vectors:
+#' \describe{
+#'   \item{gene set name}{contains gene names associated with the ChIP-seq experiment}
+#'   ...
+#' }
+#' @source \url{http://amp.pharm.mssm.edu/Enrichr/}
+"encode_example"
+
+#' ChEA gene set library
+#'
+#' May be used for genesetr examples.
+#'
+#' A list of gene sets manually curated from transcription factor
+#' ChIP-seq studies in the published literature.
+#' Downloaded from the web tool Enrichr on Aug. 23, 2017.
+#' ChEA_2015.txt
+#'
+#' @format A list of 384 character vectors:
+#' \describe{
+#'   \item{gene set name}{contains gene names associated with the ChIP-seq experiment}
+#'   ...
+#' }
+#' @source \url{http://amp.pharm.mssm.edu/Enrichr/}
+"chea_example"
