422481a01f3122c8a38015a0d1e397dcf70cdce2
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-06-13 11:49:38 -0400

    Test webhook.

---
+++ R/chea3.R
@@ -0,0 +1,33 @@
+queryChea3 = function(
+  geneset,
+  set_name = "untitled",
+  n_results = 10,
+  libnames = names(genesetr::libs),
+  integrate_libs = T,
+  method = c("FET"),
+  background = NULL){
+
+  query = list(geneset)
+  names(query) = set_name
+
+
+  if(!all(libnames %in% names(genesetr::libs))){
+    error(paste("The following libraries are unavailable:",
+      paste(setdiff(libnames,names(libs)),collapse = " "),
+      "These are the libraries available:",
+      paste(names(genesetr::libs),collapse = " ")))}
+
+  temp_libs = genesetr::libs[libnames]
+  results = lapply(temp_libs,function(x){
+    df = genesetr::pairwiseSetOverlap(query, x,
+      background = background, method = method)
+    df = df[order(df$FET.p.val),][1:n_results,]
+    return(df)
+  })
+  return(jsonlite::toJSON(results))
+}
+
+ptm <- proc.time()
+x = genesetr::queryChea3(example)
+proc.time() - ptm
+
