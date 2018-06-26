512f9ec5a802ce0c766a3bba8ac360f068671f7e
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-06-26 11:23:40 -0400

    Additional functions to map gene symbols to HGNC-approved symbols

--- R/map_gene_names.R
+++ R/map_gene_names.R
@@ -1,29 +1,30 @@
 #map genes to HGNC-approved symbols
-HGNCapproved = function(genes, untranslatable.rm = F){
+
+HGNCapproved = function(genes, untranslatable.na = F){
   message("Using HGNC 2018 approved symbols.")
-  idx = match(genes,genesetr::hgnc_dict$syn)
-  genes[!is.na(idx)] = genesetr::hgnc_dict[na.omit(idx),"approved"]
-  if(untranslatable.rm == T) genes[is.na(idx)] = NA
+  idx = match(genes,genesetr::hgnc_dict_2018$synonyms)
+  genes[!is.na(idx)] = genesetr::hgnc_dict_2018[na.omit(idx),"approved"]
+  if(untranslatable.na == T) genes[is.na(idx)] = NA
   return(genes)
 }
 
 #map all genes in a library to HGNC-approved symbols
 #should change untranslatable.rm to untranslatable.na
-lib2HGNC = function(lib, untranslatable.rm = F){
+lib2HGNC = function(lib, untranslatable.na = F){
   message("Using HGNC 2018 approved symbols.")
-  message("Note gene set names will not be mapped.")
+  message("Note gene symbols appearing in gene set names will not be mapped.")
 
   if(genesetr::isList(lib)){
     return(lapply(lib,function(set){
-      return(genesetr::HGNCapproved(set,untranslatable.rm))
+      return(genesetr::HGNCapproved(set,untranslatable.na))
     }))
 
   }else if(genesetr::isMatrix(lib)){
-    rownames(lib) = genesetr::HGNCapproved(rownames(lib),untranslatable.rm)
+    rownames(lib) = genesetr::HGNCapproved(rownames(lib),untranslatable.na)
     return(lib)
 
   }else if(genesetr::isLongDF(lib)){
-    lib$gene = HGNCapproved(lib$gene,untranslatable.rm)
+    lib$gene = HGNCapproved(lib$gene,untranslatable.na)
     return(lib)
 
   }else{
@@ -32,14 +33,17 @@ lib2HGNC = function(lib, untranslatable.rm = F){
 
 }
 
-ensembl2HGNC = function(ids){
-  if(all(grep(ids,"ENST"))){
+ensembl2HGNC = function(ids, untranslatable.na = F){
+  #get everything to ensembl gene id
+  p_match = match(ids, genesetr::ensembl_ids$ensembl_peptide_id)
+  t_match = match(ids, genesetr::ensembl_ids$ensembl_transcript_id)
 
-  }else if(all(grep(ids,"ENSG"))){
+  ids[!is.na(t_match)] = genesetr::ensembl_ids[na.omit(t_match),"ensembl_gene_id"]
+  ids[!is.na(p_match)] = genesetr::ensembl_ids[na.omit(p_match),"ensembl_gene_id"]
 
-  }else if (all(grep(ids,"ENSP"))){
-
-  }else{
-
-  }
+  #get everything to HGNC id
+  hgnc_match = match(ids,genesetr::hgnc_2018$Ensembl.ID.supplied.by.Ensembl.)
+  ids[!is.na(hgnc_match)] = genesetr::hgnc_2018[na.omit(hgnc_match), "Approved.Symbol"]
+  if(untranslatable.na) ids[is.na(hgnc_match)] = NA
+  return(ids)
 }
