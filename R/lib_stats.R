d65c839520d55506d76fd9942b162873d54ef942
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-06-26 11:25:32 -0400

    Quickly get gene set library statistics.

---
+++ R/lib_stats.R
@@ -0,0 +1,19 @@
+getLibInfo = function(lib){
+  lib = genesetr::toList(lib)
+  lib_lengths = unlist(lapply(lib,length))
+  quart = as.numeric(quantile(lib_lengths))
+  return(data.frame(
+    n_gene_sets = length(lib),
+    n_unique_genes = length(unique(unlist(lib))),
+    mean_lib_length = mean(lib_lengths),
+    sd_lib_length = sd(lib_lengths),
+    quartile1_lib_length = quart[1],
+    quartile2_lib_length = quart[2],
+    quartile3_lib_length = quart[3],
+    quartile4_lib_length = quart[4]
+  ))
+}
+
+getLibGeneFreqs = function(lib){
+  gene_tab = as.data.frame(table(unlist(lib)))
+}
