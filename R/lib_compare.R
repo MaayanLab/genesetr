4dd583f314d5c18172b09c9791207f080d1f48a8
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-07-23 11:06:39 -0400

    Remove library weights prior to comparison.

--- R/lib_compare.R
+++ R/lib_compare.R
@@ -1,7 +1,9 @@
 
-libCompare = function(lib1,lib2, method = c("jaccard index")){
+libCompare = function(lib1,lib2, method = "jaccard"){
+  lib1 = genesetr::removeLibWeights(lib1)
+  lib2 = genesetr::removeLibWeights(lib2)
   results = genesetr::pairwiseContingTables(lib1, lib2, background = 20000)
-  if(method == "jaccard index"){
+  if("jaccard" %in% method){
     results$jaccard = genesetr::jaccardInd(results$a, results$b, results$c, results$d)
     return(results)
   }else{
