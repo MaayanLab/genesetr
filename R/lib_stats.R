bfb4598ecdddcf47e389245e82aef99bebf5f61e
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-08-03 14:07:37 -0400

    Fixed lib stats function to remove lib weights before calculating stats.

--- R/lib_stats.R
+++ R/lib_stats.R
@@ -1,16 +1,17 @@
 getLibInfo = function(lib){
   lib = genesetr::toList(lib)
-  lib_lengths = unlist(lapply(lib,length))
+  lib = genesetr::removeLibWeights(lib)
+  set_lengths = unlist(lapply(lib,length))
   quart = as.numeric(quantile(lib_lengths))
   return(data.frame(
     n_gene_sets = length(lib),
     n_unique_genes = length(unique(unlist(lib))),
-    mean_lib_length = mean(lib_lengths),
-    sd_lib_length = sd(lib_lengths),
-    quartile1_lib_length = quart[1],
-    quartile2_lib_length = quart[2],
-    quartile3_lib_length = quart[3],
-    quartile4_lib_length = quart[4]
+    mean_lib_length = mean(set_lengths),
+    sd_set_length = sd(set_lengths),
+    quartile1_set_length = quart[1],
+    quartile2_set_length = quart[2],
+    quartile3_set_length = quart[3],
+    quartile4_set_length = quart[4]
   ))
 }
 
